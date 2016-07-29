c0196c96bc6a1cd6dd61dcc75cebb5efa8aa3ba5
Christopher Wong <chriskwong91@gmail.com>
2016-07-28 20:23:52 -0700

    functional shared queue and stack implemented and also linked list

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackobj = {};
+  _(stackobj).extend(stackMethods);
+  stackobj.storage = {};
+  return stackobj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
+	  var index = this.size();
+	  this.storage[index] = value;
+	},
+	pop: function() {
+	  var index = this.size() - 1;
+	  var temp = this.storage[index];
+	  delete this.storage[index];
+	  return temp;
+	},
+	size: function() {
+		return Object.keys(this.storage).length;
+	}
+};
 
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      var initialTail = new Node(value);
+      list.head = initialTail;
+      list.tail = initialTail;
+    } else {
+      var newTail = new Node(value);
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
+    var newHead = list.head.next;
+    var oldHeadValue = list.head.value;
+    delete list.head;
+    list.head = newHead;
+    return oldHeadValue;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    
+    while (true) {
+      if (current.value === target) {
+        return true;
+      } 
+      if (current.next === null) {
+        return false;
+      }
+      current = current.next;
+    }
   };
 
   return list;
